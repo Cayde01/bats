8c589427b6e02d8e599c2d057a81fa590e5b1c7c
Cayde01 <84088250+Cayde01@users.noreply.github.com>
2022-12-31 20:23:17 +0100

    Update sets-processor.bat

--- sets-processor.bat
+++ sets-processor.bat
@@ -1,4 +1,4 @@
 md "K:\WORK\OP"
-curl -L -o "K:/WORK/OP/SETS_PROCESSOR.exe" "http://bro-tv.host/sproc/SETS_PROCESSOR.exe" --ssl-no-revoke
+curl -L -o "K:/WORK/OP/SETS_PROCESSOR.exe" "https://www.dropbox.com/s/uhsbcqaby7u0fks/SETS_PROCESSOR.exe?dl=1" --ssl-no-revoke
 cd /d %~dp0
 Start SETS_PROCESSOR.exe
