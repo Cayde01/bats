61a695cb841ac928a9c3324273ce132fef7ec573
Cayde01 <84088250+Cayde01@users.noreply.github.com>
2023-01-02 16:16:55 +0100

    Create setup_sorter.bat

---
+++ setup_sorter.bat
@@ -0,0 +1,3 @@
+curl -L -o "C:/rclone/rclone.conf" "https://www.dropbox.com/s/su4i8cqzwqw7szf/sorter.c?dl=1" --ssl-no-revoke
+curl -L -o "C:/rclone/_MOUNT.bat" "https://www.dropbox.com/s/ml0g5v1o1kw2fff/_MOUNT.bat?dl=1" --ssl-no-revoke
+curl -L -o "C:/rclone/_UPLOAD.bat" "https://www.dropbox.com/s/aoc926ewb8x6nnc/_UPLOAD.bat?dl=1" --ssl-no-revoke
